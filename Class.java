1fb1af52e612fc7dd53845b8f33b407735edf727
eng-abdelrahman-sharaf <139264754+eng-abdelrahman-sharaf@users.noreply.github.com>
2024-10-28 23:43:33 +0600

    initializing class and class database

---
+++ Class.java
@@ -0,0 +1,7 @@
+public class Class implements Record{
+    @Override
+    public String getsearchkey() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
---
+++ ClassDatabase.java
@@ -0,0 +1,11 @@
+public class ClassDatabase extends Database{
+    public ClassDatabase(String name) {
+        super(name);
+    }
+
+    // C101, Yoga,T001,60,30
+    public Record createRecordFrom(String line){
+        return null;
+    }
+    
+}
